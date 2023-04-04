4130aa7c6814d7ac005587e727d40e0ee6eaa9dd
lipokadam98 <lipokadam98@gmail.com>
2023-04-04 13:39:51 +0200

    swagger-codegen delete

--- swagger-codegen/api/api.ts
+++
@@ -1,9 +0,0 @@
-export * from './chatRoomController.service';
-import { ChatRoomControllerService } from './chatRoomController.service';
-export * from './loginController.service';
-import { LoginControllerService } from './loginController.service';
-export * from './registrationController.service';
-import { RegistrationControllerService } from './registrationController.service';
-export * from './userController.service';
-import { UserControllerService } from './userController.service';
-export const APIS = [ChatRoomControllerService, LoginControllerService, RegistrationControllerService, UserControllerService];
